5644c3f32cc5351d40b0e5f4005ccd938d344f53
verbosesecurity <42240122+verbosesecurity@users.noreply.github.com>
2020-07-24 20:09:39 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+log="/var/log/rsync/$(date +"%Y_%m_%d_%H_%M").log"
+sources=(
+        "/foo/directory" 
+        )
+for source in "${sources[@]}"; do
+    touch "$log"
+    rsync -av "$source" --log-file="$log" /foo/destination
+    done
