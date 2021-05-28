e6988d0e45aca2a62a6c57d2cf449b21e5b54d63
WhiteMastiff <42240122+WhiteMastiff@users.noreply.github.com>
2021-05-28 17:43:17 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ log="/var/log/rsync/$(date +"%Y_%m_%d_%H_%M").log"
 sources=(
         "/foo/directory" 
         )
+touch "$log"
 for source in "${sources[@]}"; do
-    touch "$log"
     rsync -ai "$source" --log-file="$log" /foo/destination
     done
